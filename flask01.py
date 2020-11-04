6d7ed0cdfb3ef6f8d5ea2855fb15bc57ebf4df89
Cameron Bhatnagar <cbhatnag@uncc.edu>
2020-11-04 13:32:44 -0500

    Added log in page

--- flask01.py
+++ flask01.py
@@ -13,9 +13,13 @@ app = Flask(__name__)     # create an app
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Cameron Bhatnagar', 'email': 'cbhatnag@uncc.edu'}
 
-    return render_template('index.html', user = a_user)
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+
+    return render_template('login.html')
 
 @app.route('/notes')
 def get_notes():
